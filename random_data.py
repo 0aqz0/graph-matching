59cbeac52a777a9d48076c6b75c95059ef9b4f64
luckky <guodashun@163.com>
2020-10-08 18:48:16 +0800

    gen dataset

--- random_data.py
+++ random_data.py
@@ -1,31 +1,38 @@
 import matplotlib.pyplot as plt
 from matplotlib import patches
 import numpy as np
+import torch
+import pickle
+import json
 import random
 import math
+import os
 
-classes_num = 6
+# classes_num = 6
 labels = ["tree", "car", "bike", "pedestrain", "well", "light"]
 colors = ["green", "black", "royalblue", "bisque", "grey", "yellow"]
-total_num = 30
+total_num = 64
 min_x = 0
-max_x = 100
+max_x = 640
 min_y = 0
-max_y = 100
+max_y = 480
 noise_mu = 0
 noise_var = 2.0
-visual_range = 20.0
+visual_range = 160.0
 visual_theta = 60.0 # degree
+save_path = "./dataset/" + str(max_x) + "x" + str(max_y) + "_" + str(total_num)
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
 
 def create_global_map():
     global_map = []
     for _ in range(total_num):
         random_x = random.random() * (max_x - min_x) + min_x
         random_y = random.random() * (max_y - min_y) + min_y
-        random_label = random.randint(0, classes_num-1)
+        random_label = random.randint(0, len(labels)-1)
         landmark = [random_x, random_y, random_label]
         global_map.append(landmark)
-
     return global_map
 
 
@@ -57,20 +64,23 @@ def create_local_map(map):
     translation = np.expand_dims(translation, axis=1)
     rotation = np.array([[np.cos(random_ori-math.pi/2), np.sin(random_ori-math.pi/2)],
                         [-np.sin(random_ori-math.pi/2), np.cos(random_ori-math.pi/2)]])
+    
     local_map = []
-    for landmark in map:
+    match = []
+    for i, landmark in enumerate(map):
         global_pose = np.array([landmark[0], landmark[1]]).T
         global_pose = np.expand_dims(global_pose, axis=1)
-        local_pose = rotation @ (global_pose + translation)
-        local_x = local_pose[0]
-        local_y = local_pose[1]
+        local_pose = (rotation @ (global_pose + translation))
+        local_x = local_pose[0][0]
+        local_y = local_pose[1][0]
         local_label = landmark[2]
         local_landmark = [local_x, local_y, local_label]
         theta = math.atan2(local_landmark[1], local_landmark[0]) / math.pi * 180.0
-        if math.hypot(local_x, local_y) < visual_range and theta > 90 - visual_theta and theta < 90 + visual_theta :
+        if math.hypot(local_x, local_y) < visual_range and theta > 90 - visual_theta and theta < 90 + visual_theta:
             local_map.append(local_landmark)
+            match.append(i)
         
-    return base_pose, local_map
+    return base_pose, local_map, match
 
 
 def visualize_all(global_map, local_map, base_pose):
@@ -107,17 +117,74 @@ def visualize_all(global_map, local_map, base_pose):
     plt.show()
 
 
+def gen_dataset():
+
+    # keypoints           list(float(x,y))
+    # scores              float(1.)
+    # descriptors         list(int[0,len(labels)]*total_nums)
+    # matches             list(float(x,y))      
+    # matching_scores     list(float(1.))
+    # base_pose           float(x,y)
+
+    # init raw data
+    gm = create_global_map()
+    bp, lm, match = create_local_map(gm)
+    # make sure that lm is not empty
+    while(len(lm)==0):
+        bp, lm, match = create_local_map(gm)
+    gm = add_gaussian_noise(gm)
+
+    # base map
+    gsize = len(gm)
+    keypoints0 = torch.from_numpy(np.array(gm)[:, 0:2])
+    scores0 = torch.ones(gsize)
+    descriptors0 = torch.Tensor([[i[2]] for i in gm])
+    matches0 =[]
+    index = 0
+    for i in range(gsize):
+        if i in match:
+            matches0.append(index)
+            index = index+1
+        else:
+            matches0.append(-1)
+    matches0 = torch.Tensor(matches0)
+    # matches0 = torch.Tensor([i in match and i or -1 for i in range(gsize)])
+    matching_scores0 = torch.Tensor([i > -1 and 1. or 0. for i in range(gsize)])
+
+    # match map
+    lsize = len(lm)
+    keypoints1 = torch.from_numpy(np.array(lm)[:, 0:2])
+    scores1 = torch.ones(lsize)
+    descriptors1 = torch.Tensor([[i[2]] for i in lm])
+    matches1 = match
+    matching_scores1 = torch.ones(lsize)
+    
+    data = dict({'keypoints0': keypoints0, 'keypoints1': keypoints1,
+                 'scores0': scores0, 'scores1': scores1,
+                 'descriptors0': descriptors0, 'descriptors1': descriptors1,
+                 'matches0': matches0, 'matches1': matches1,
+                 'matching_scores0': matching_scores0, 'matching_scores1': matching_scores1,
+                 'base_pose': bp[0:2]})
+
+    return data
+
+
 if __name__ == '__main__':
     # create global map
-    global_map = create_global_map()
+    # global_map = create_global_map()
     # visualize_map(global_map)
     
     # add noise
-    noise_map = add_gaussian_noise(global_map)
+    # noise_map = add_gaussian_noise(global_map)
     # visualize_map(noise_map)
 
     # create local map
-    base_pose, local_map = create_local_map(global_map)
+    # base_pose, local_map, _= create_local_map(global_map)
 
     # visualize all
-    visualize_all(global_map, local_map, base_pose)
+    # visualize_all(global_map, local_map, base_pose)
+
+    # print(gen_dataset())
+    for i in range(10000):
+        # pickle.dump(gen_dataset(), open(os.path.join(save_path, str(i).zfill(4)+".pkl"), "wb"))
+        np.save(os.path.join(save_path, str(i).zfill(4)+".npy"), gen_dataset())
